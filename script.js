1f4c9626a5ecfbe7220b69ab2246f7b2c5a379bd
sheepman39 <jsalata90@gmail.com>
2021-08-25 14:23:48 -0500

    Added comments and cleaned up code

--- script.js
+++ script.js
@@ -1,114 +1,131 @@
+// Change this to the teacher name
+var teacher = "mendoza";
 
-            var teacher = "mendoza";
-            /*
-            Each choices object has two things:
-                An array with student names
-                An extra array that stores the students that were selected
-            Each extra array allows for flexibility and modularity and restartability for the program
-            */
-            function dynamicButtons(data) {
-                try {
-                        
-                        for (classHour in data[teacher]) {
-                            
-                            var button = document.createElement("BUTTON");
-                            button.setAttribute("id", classHour);
-                            button.innerHTML = data[teacher][classHour].className ? data[teacher][classHour].className : classHour;
+// dynamicButtons receives the data from the json file and will make a new button for each class
+function dynamicButtons(data) {
+    
+    // in case if anything goes wrong, try/catch block for error reporting
+    try {
+        
+        // loop that will take each hour from the teacher specified in line 2
+        for (classHour in data[teacher]) {
+            
+            // creates the button element and sets the id as the value of the classHour
+            var button = document.createElement("BUTTON");
+            button.setAttribute("id", classHour);
+            
+            // If a specified className is specified in students.json, use that as the name
+            // If no className is defined, use the default class hour name as the text
+            button.innerHTML = data[teacher][classHour].className ? data[teacher][classHour].className : classHour;
 
-                            // makes the button visible on the body
-                            document.getElementById("container").appendChild(button);
+            // makes the button visible on the body
+            document.getElementById("container").appendChild(button);
 
-                            
-                            button.addEventListener("click", function (classHour) {
-                                return function(){
-                                    studentSelector(data[teacher][classHour]);
-                                }
-                            }(classHour));
-                        }
-                } catch (err) {
-                    document.getElementById("debug").innerHTML = err;
-                    console.log(err);
-                }
-            }
-            let Classes;
-
-            fetch('./students.json')
-                .then(function (response) {
-                    response.clone().json();
-                    return response.json();
-                })
-                .then(function (data) {
-
-                    Classes = data;
-                    //stringClasses = JSON.stringify(Classes, null, 4);
-                    //document.getElementById("debug").innerHTML = "Classes: " + stringClasses;
-                    //dynamicButtons(data);
-                    return data;
-
-                })
-                .then(function (data) {
-
-                    document.addEventListener("DOMContentLoaded", dynamicButtons(Classes));
+            // adds the event listener to each of the buttons
+            // because variable will change, this needs to be a local function with local variable scope
+            button.addEventListener("click", function (classHour) {
+                return function(){
                     
-
-                })
-                .catch(function (err) {
-                    console.log(err);
-                    document.getElementById("debug").innerHTML = `Ops something went wrong.  Please report the following error code:\n${err}`;
-                });
-
-
-            // this function resets all of the names to be reused
-            function masterRestart(ClassObject) {
-
-                // this takes each class hour extra array and pushes it back into the names
-                for (classHour in ClassObject) {
-                    ClassObject[classHour].extra.forEach(name => {
-                        ClassObject[classHour].names.push(name);
-                    });
-                    ClassObject[classHour].extra = [];
+                    // student selector is the function to select students
+                    studentSelector(data[teacher][classHour]);
                 }
+            }(classHour));
+        }
+        
+    } catch (err) {
+        
+        // if there are errors, print them out 
+        document.getElementById("debug").innerHTML = `Ops something went wrong.  Please report the following error code:\n${err}`;
+        console.log(err);
+        
+    }
+}
 
-                // Displays a message that the classes are restarted
-                document.getElementById("output").innerHTML = "All classes are restarted";
-                document.getElementById("remaining").innerHTML = "Please pick a class";
-                document.getElementById("numberRemaining").innerHTML = "";
+// Defines Classes as a variable that will later have json data assigned to it
+let Classes;
 
-            }
+// fetch the json data from this file
+fetch('./students.json')
+    .then(function (response) {
+        
+        // clones the json response and returns that data
+        response.clone().json();
+        return response.json();
+    })
+    .then(function (data) {
+        
+        // sets the response data to the Classes object
+        Classes = data;
+        // debug code for json data
+        //stringClasses = JSON.stringify(Classes, null, 4);
+        //document.getElementById("debug").innerHTML = "Classes: " + stringClasses;
+        //dynamicButtons(data);
+        return data;
 
-            // this is the random number generator
-            function getRndInteger(min, max) {
-                return Math.floor(Math.random() * (max - min)) + min;
-            }
-            // this is the actual selector function
-            function studentSelector(hour) {
+    })
+    .then(function (data) {
 
-                // checks if the array is empty
-                if (hour.names.length === 0) {
+        // when the DOMContentLoaded is true, start making the buttons
+        document.addEventListener("DOMContentLoaded", dynamicButtons(Classes));
 
-                    // if the array is empty, push those names into the main array
-                    hour.extra.forEach(name => {
-                        hour.names.push(name)
-                    });
+    })
+    .catch(function (err) {
+        console.log(err);
+        document.getElementById("debug").innerHTML = `Ops something went wrong.  Please report the following error code:\n${err}`;
+    });
 
-                    // clears the extra array
-                    hour.extra = [];
 
-                }
+// this function resets all of the names to be reused
+function masterRestart(ClassObject) {
 
-                // ranNum picks a random number between 0 and the length of the array
-                // pick stores the name that is randomly selected
-                var ranNum = getRndInteger(0, hour.names.length);
-                var pick = hour.names[ranNum];
+    // this takes each class hour extra array and pushes it back into the names
+    for (classHour in ClassObject) {
+        ClassObject[classHour].extra.forEach(name => {
+            ClassObject[classHour].names.push(name);
+        });
+        ClassObject[classHour].extra = [];
+    }
 
-                // The chosen name is pushed to the extra array
-                // then that name is removed from the names array so it is not picked again
-                hour.extra.push(pick);
-                hour.names.splice(ranNum, 1);
+    // Displays a message that the classes are restarted
+    document.getElementById("output").innerHTML = "All classes are restarted";
+    document.getElementById("remaining").innerHTML = "Please pick a class";
+    document.getElementById("numberRemaining").innerHTML = "";
 
-                // output of the selected student, students left, and the number left
-                document.getElementById("output").innerHTML = "<b><em>Next Victim:</em></b>" + pick;
-                document.getElementById("remaining").innerHTML = "Students left: " + hour.names;
-                document.getElementById("numberRemaining").innerHTML = "Number of students remaining: " + hour.names.length;
+}
 
-            }
+// this is the random number generator
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+// this is the actual selector function
+function studentSelector(hour) {
+
+    // checks if the array is empty
+    if (hour.names.length === 0) {
+
+        // if the array is empty, push those names into the main array
+        hour.extra.forEach(name => {
+            hour.names.push(name)
+        });
+
+        // clears the extra array
+        hour.extra = [];
+
+    }
+
+    // ranNum picks a random number between 0 and the length of the array
+    // pick stores the name that is randomly selected
+    var ranNum = getRndInteger(0, hour.names.length);
+    var pick = hour.names[ranNum];
+
+    // The chosen name is pushed to the extra array
+    // then that name is removed from the names array so it is not picked again
+    hour.extra.push(pick);
+    hour.names.splice(ranNum, 1);
+
+    // output of the selected student, students left, and the number left
+    document.getElementById("output").innerHTML = "<b><em>Next Victim:</em></b>" + pick;
+    document.getElementById("remaining").innerHTML = "Students left: " + hour.names;
+    document.getElementById("numberRemaining").innerHTML = "Number of students remaining: " + hour.names.length;
+
+}
